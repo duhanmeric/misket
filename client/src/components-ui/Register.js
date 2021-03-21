5ff494744bc6ea74cdf9bcfe4b2514acc0f8625e
Duhan Meric <duhanmeric@gmail.com>
2021-03-21 22:13:34 +0300

    register page design

--- client/src/components-ui/Register.js
+++ client/src/components-ui/Register.js
@@ -29,26 +29,59 @@ export default function Login() {
   };
 
   return (
-    <div className="container d-flex flex-column w-50">
-      <div className="text-center mb-3">Register Page</div>
-      <input type="text" placeholder="email" ref={email} />
-      <input
-        type="text"
-        className="mt-3"
-        placeholder="username"
-        ref={username}
-        required
-      />
-      <input
-        type="text"
-        className="my-3"
-        placeholder="password"
-        ref={password}
-        required
-      />
-      <button className="btn btn-dark" onClick={() => handleReq()}>
-        Register
-      </button>
+    <div className="container w-75">
+      <h1 className="register-title text-center mb-5">Sign In</h1>
+      <div className="form-group mx-auto" style={{ maxWidth: "300px" }}>
+        <label htmlFor="email" className="mb-1">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          aria-describedby="registerEmail"
+          placeholder="Enter email"
+          ref={email}
+          required
+        />
+      </div>
+      <div className="form-group mx-auto my-3" style={{ maxWidth: "300px" }}>
+        <label htmlFor="username" className="mb-1">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          aria-describedby="registerUsername"
+          placeholder="Enter username"
+          ref={username}
+          required
+        />
+      </div>
+      <div className="form-group mx-auto" style={{ maxWidth: "300px" }}>
+        <label htmlFor="password" className="mb-1">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          aria-describedby="registerPassword"
+          placeholder="Enter password"
+          ref={password}
+          required
+        />
+      </div>
+      <div className="btn-container d-flex justify-content-center mt-4">
+        <button
+          className="make-list-btn"
+          onClick={() => handleReq()}
+          style={{ maxWidth: "300px" }}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   );
 }
