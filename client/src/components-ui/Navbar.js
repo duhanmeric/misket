f468168f7adf7d6f597c25f15543ddf9e10ef594
Duhan Meric <duhanmeric@gmail.com>
2021-03-25 20:02:03 +0300

    dark mode switch func

--- client/src/components-ui/Navbar.js
+++ client/src/components-ui/Navbar.js
@@ -1,9 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../UserProvider";
 
 export default function Navbar() {
   const { user } = useContext(UserContext);
+  const [theme, setTheme] = useState("light");
+  const handleTheme = (value) => {
+    switch (value) {
+      case 0:
+        setTheme("dark"); // dark
+        break;
+      case 1:
+        setTheme("light"); // light
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    console.log("run use effect");
+    document.documentElement.className = theme;
+  }, [theme]);
 
   return (
     <header className="fixed-top w-100 d-flex justify-content-between">
@@ -12,6 +30,16 @@ export default function Navbar() {
       </div>
 
       <div className="auth d-flex justify-content-between align-items-center">
+        {theme === "light" ? (
+          <div className="dark-btn" onClick={() => handleTheme(0)}>
+            <i className="far fa-moon"></i>
+          </div>
+        ) : (
+          <div className="light-btn" onClick={() => handleTheme(1)}>
+            <i className="far fa-sun"></i>
+          </div>
+        )}
+
         {user ? (
           <div className="profile-link">
             <Link to={`/dashboard/${user.username}`}>Dashboard</Link>
