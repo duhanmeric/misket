3d8ab2aff7a04c82aa402e9531bd85cc6a7ea8bb
Duhan Meric <duhanmeric@gmail.com>
2021-03-27 16:44:49 +0300

    register responsive

--- client/src/components-ui/Register.js
+++ client/src/components-ui/Register.js
@@ -29,58 +29,65 @@ export default function Login() {
   };
 
   return (
-    <div className="container w-75">
-      <h1 className="register-title text-center mb-5">Sign In</h1>
-      <div className="form-group mx-auto" style={{ maxWidth: "300px" }}>
-        <label htmlFor="email" className="mb-1">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          aria-describedby="registerEmail"
-          placeholder="Enter email"
-          ref={email}
-          required
-        />
-      </div>
-      <div className="form-group mx-auto my-3" style={{ maxWidth: "300px" }}>
-        <label htmlFor="username" className="mb-1">
-          Username
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="username"
-          aria-describedby="registerUsername"
-          placeholder="Enter username"
-          ref={username}
-          required
-        />
-      </div>
-      <div className="form-group mx-auto" style={{ maxWidth: "300px" }}>
-        <label htmlFor="password" className="mb-1">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          aria-describedby="registerPassword"
-          placeholder="Enter password"
-          ref={password}
-          required
-        />
-      </div>
-      <div className="btn-container d-flex justify-content-center mt-4">
-        <button
-          className="make-list-btn"
-          onClick={() => handleReq()}
-          style={{ maxWidth: "300px" }}
-        >
-          Sign In
-        </button>
+    <div className="register">
+      <div className="container w-75">
+        <div className="register-content">
+          <h1 className="register-title text-center mb-5">Sign In</h1>
+          <div className="form-group mx-auto" style={{ maxWidth: "300px" }}>
+            <label htmlFor="email" className="mb-1">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="registerEmail"
+              placeholder="Enter email"
+              ref={email}
+              required
+            />
+          </div>
+          <div
+            className="form-group mx-auto my-3"
+            style={{ maxWidth: "300px" }}
+          >
+            <label htmlFor="username" className="mb-1">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              aria-describedby="registerUsername"
+              placeholder="Enter username"
+              ref={username}
+              required
+            />
+          </div>
+          <div className="form-group mx-auto" style={{ maxWidth: "300px" }}>
+            <label htmlFor="password" className="mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              aria-describedby="registerPassword"
+              placeholder="Enter password"
+              ref={password}
+              required
+            />
+          </div>
+          <div className="btn-container d-flex justify-content-center mt-4">
+            <button
+              className="make-btn"
+              onClick={() => handleReq()}
+              style={{ maxWidth: "300px", border: "none", width: "100%" }}
+            >
+              Sign In
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
