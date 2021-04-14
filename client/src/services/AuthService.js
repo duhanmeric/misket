44bf288e002325936a9ef54fb308ea1bfaa7fb74
Duhan Meric <duhanmeric@gmail.com>
2021-04-14 19:10:44 +0300

    deploy

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -8,7 +8,6 @@ export default {
     return Api().post("/login", credentials);
   },
   verification({ confirmationTicket }) {
-    console.log(confirmationTicket);
     return Api().get(`/verification/${confirmationTicket}`);
   },
 };
