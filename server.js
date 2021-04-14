b2d5ecb6031cf0f009912e627a11987d99fbab53
Duhan Meric <duhanmeric@gmail.com>
2021-04-14 19:26:00 +0300

    deploy

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const { sequelize } = require("./src/models");
 const path = require("path");
 const config = require("./src/config/config");
+const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
 
 const app = express();
 app.use(express.json());
@@ -10,9 +12,18 @@ app.use(cors());
 
 require("./src/routes")(app);
 
+const staticFileMiddleware = serveStatic(path.join(__dirname + "/build"));
+app.use(staticFileMiddleware);
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true,
+  })
+);
+app.use(staticFileMiddleware);
+
 app.get("/", (req, res) => {
-  app.use(express.static(path.join(__dirname, "./build")));
-  res.render(path.join(__dirname, "./build/index.html"));
+  res.render(path.join(__dirname, "/build/index.html"));
 });
 
 sequelize.sync().then(() => {
